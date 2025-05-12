6e411ca0167e1b06401b3635f16e9a400873c142
Lancia-Stratos <mktymail@gmail.com>
2025-05-12 17:52:51 +0900

    unitページのマージンを調整

--- .history/src/app/(authenticated)/units/layout_20250512174734.tsx
+++ .history/src/app/(authenticated)/units/layout_20250512174734.tsx
@@ -2,7 +2,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
     return (
         <div>
-            <h1 className="text-2xl font-bold mb-2 ml-0.5">単位管理</h1>
+            <h1 className="text-2xl font-bold mb-2.5 ml-0.5">単位管理</h1>
             {children}
         </div>
     );
---
+++ .history/src/components/app-sidebar_20250426175658.tsx
@@ -0,0 +1,100 @@
+import { Calendar, Home, Ruler, Search, Settings } from "lucide-react"
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarFooter,
+    SidebarTrigger,
+    SidebarSeparator,
+} from "@/components/ui/sidebar"
+import { metadata } from "@/app/layout"
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "/",
+        icon: Home,
+    },
+    {
+        title: "単位",
+        url: "/units",
+        icon: Ruler,
+    },
+    {
+        title: "単位保存テスト",
+        url: "/units/add",
+        icon: Calendar,
+    },
+    {
+        title: "Search",
+        url: "#",
+        icon: Search,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+    },
+]
+
+export function AppSidebar() {
+
+    return (
+        <Sidebar collapsible="icon">
+            <SidebarContent>
+
+                <SidebarHeader className="ml-0.5">
+
+                    <div className="flex items-center">
+                        {/* サイドバーの開閉ボタン */}
+                        <SidebarTrigger />
+                        {/* サイドバーのタイトル */}
+                        <SidebarGroupLabel className="!ml-0.5 !pl-0">
+                            {String(metadata.title)}
+                        </SidebarGroupLabel>
+                    </div>
+
+                </SidebarHeader>
+
+                <SidebarSeparator />
+
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu className="">
+                            {/* メニューのアイテム*/}
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+
+                {/* ログインしている時はユーザーアイコンを表示 */}
+                <div className="mt-auto">
+                    <SidebarFooter>
+                        <div className="mt-auto mb-20 flex w-full group-data-[collapsible=icon]:justify-center group-data-[state=expanded]:ml-2">
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
+                    </SidebarFooter>
+                </div>
+            </SidebarContent >
+        </Sidebar >
+    )
+}
+
